38bccc6efa90e187bd4efd4200328bc03016e945
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2024-05-08 21:48:57 +0530

    Aigis SL Files Upload Part 1

---
+++ UnrealEssentials/P3R/Content/L10N/en/Xrd777/Events/Cinema/Event_Cmmu_007_047_C/BMD_Event_Cmmu_007_047_C_unwrapped.bmd.msg.h
@@ -0,0 +1,14 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int MSG_000_3_0                      = 1;
+const int SEL_001_0_0                      = 2;
+const int MSG_002_0_0                      = 3;
+const int MSG_002_3_0                      = 4;
+const int MSG_002_5_0                      = 5;
+const int MSG_003_0_0                      = 6;
+const int MSG_003_3_0                      = 7;
+const int MSG_004_0_0                      = 8;
+const int MSG_004_3_0                      = 9;
+const int MND_005_0_0                      = 10;
+const int MSG_006_0_0                      = 11;
+const int MND_007_0_0                      = 12;
